8958e68db9c8a71c30f586c5a8a7b1d990ec7e11
SohamTcog <soham.kulkarni@tcognition.com>
2024-01-15 11:51:32 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css';
 import Display from './Components/Display';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LinkPage from './Components/LinkPage';
 function App() {
   return (
     <div className="App">
          <h1>React API Demo</h1>
-      <Display/>
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LinkPage/>}  />
+      <Route path="/display" element={  <Display/>} />
+     
+  </Routes>
+  </BrowserRouter>
+      
+        
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/LinkPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const LinkPage = () => {
+  return (
+    <div> <Link to="/display"  color="primary">
+    Data
+  </Link></div>
+  )
+}
+
+export default LinkPage;
