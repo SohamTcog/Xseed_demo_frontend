607a30d23cf0b9ec91181fa04fb83991b1a145fb
Roshan-Farkate <138759751+Roshan-Farkate@users.noreply.github.com>
2024-01-19 16:13:44 +0530

    Update App.test.tsx 9

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,18 +3,30 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import LinkPage from './Components/LinkPage';
 //import LinkPage from '../Components/LinkPage';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  Link: ({ to, children }: { to: string; children: React.ReactNode }) => (
-    <a href={to}>{children}</a>
-  ),
-}));
+// jest.mock('react-router-dom', () => ({
+//   ...jest.requireActual('react-router-dom'),
+//   Link: ({ to, children }: { to: string; children: React.ReactNode }) => (
+//     <a href={to}>{children}</a>
+//   ),
+// }));
 
-describe('LinkPage component', () => {
-  test('does not navigate when Data link is clicked', () => {
-    render(<LinkPage />);
+// describe('LinkPage component', () => {
+//   test('does not navigate when Data link is clicked', () => {
+//     render(<LinkPage />);
 
-    const dataLink = screen.getByText('Data');
-    fireEvent.click(dataLink);
-  });
+//     const dataLink = screen.getByText('Data');
+//     fireEvent.click(dataLink);
+//   });
+// });
+
+
+test('renders LinkPage component', () => {
+  const { getByText } = render(
+    <Router>
+      <LinkPage />
+    </Router>
+  );
+
+  const linkElement = getByText(/Data/i);
+  expect(linkElement).toBeInTheDocument();
 });
